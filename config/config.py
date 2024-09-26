204a8f9339aa0f132b53a939694a0b13513edd9a
Denglvch <biza44@mail.ru>
2024-09-26 17:46:55 +0400

    edit-routes

--- config/config.py
+++ config/config.py
@@ -4,6 +4,6 @@ DB_URL = os.getenv('DB_URL', 'postgresql+asyncpg://kramer:EVvbuGcRqM4DopujH4x5VS
 SECRET_KEY = '456'
 JWT_ALGORITHM = "HS256"
 HASH_SECRET_KEY = '3OjRMH4i2V8QF6k4BqRfXSGUB'
-AUTH_BY_TOKEN = False
+AUTH_BY_TOKEN = True
 APP_BOT_TOKEN = os.getenv('APP_BOT_TOKEN', '7289320383:AAErCSgKU9zFPCrWVaUBh8oH1LMOuJ-8ovU')
 DATE_FORMAT: str = "%d-%m-%Y %H:%M"
