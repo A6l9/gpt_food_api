410d1b736e44dc305d678ed4e7ece3082dc46eb1
A6l9 <git@github.com:A6l9>
2024-09-30 21:24:31 +0500

    fix

--- api/routes.py
+++ api/routes.py
@@ -201,14 +201,15 @@ async def save_diary(
     gpt_token = (await dbconf.get_setting('gpt_token')).get_value()
     gpt_promt = (await dbconf.get_setting('gpt_promt')).get_value()
     gpt = GPT(token=gpt_token, promt=gpt_promt)
+    logger.debug('ПРОБЛЕМА!!!!!!!!')
     try:
         await gpt.sub_request(request.text, db, user.id, reformat_date(datetime.datetime.utcnow(), int(request.timezone))) #,path_to_photo=temp.path_to_photo
         response_data = {
             'data': 'Записано'
         }
         return response_data
-    except HTTPException as exc:
-        logger.error(exc)
+    except Exception as exc:
+        logger.exception(exc)
         response_data = {
             'data': 'Ошибка записи'
         }
